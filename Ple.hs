7d4fd7d5fbefd2931b05518072ae271f12ea38fd
Mustafa Hafidi <mustafahafidi96@gmail.com>
2020-05-30 03:44:14 +0200

    Update Ple.hs

--- Ple.hs
+++ Ple.hs
@@ -1,15 +1,36 @@
-{-@ LIQUID "--ple-local"    @-}
-{-@ LIQUID "--reflection"    @-}
+{-@ LIQUID "--ple-local" @-}
+-- @ LIQUID "--exactdc" @
 
-{-@ reflect anyM @-}
-anyM :: (a -> Bool) -> [a] -> Bool
-anyM _ []        = False
-anyM p (x:xs)    = p x || anyM p xs 
 
-{-@ reflect asd @-}
-asd :: Int -> [Int] -> Bool
-asd a b = anyM (\x -> a == x) b
+data CList a = Empty
+             | CList [a] a [a]
 
-{-@ ple pr @-}
-{-@ pr :: { asd 1 [1] } @-}
-pr = ()
+
+{-@ reflect singleton @-}
+singleton :: a -> CList a
+singleton e = CList [] e [] 
+
+{-@ reflect toList @-}
+toList :: CList a -> [a]
+toList Empty = []
+toList (CList l f r) = f : (r ++ (reverse l))
+
+
+{-@ reflect eqToList @-}
+eqToList ::  CList Int -> CList Int -> Bool
+eqToList a b = toList a == toList b
+
+{-@ reflect =*= @-}
+{-@ infix 4 =*= @-}
+(=*=) :: CList Int -> CList Int -> Bool
+x =*= y = (any (eqToList x) (toList (singleton y)))
+
+{-@ reflect lemma_refl @-}
+lemma_refl :: Bool
+lemma_refl = Empty =*= Empty
+
+{-@ ple lemma_refl_proof @-}
+{-@ lemma_refl_proof ::  { lemma_refl  } @-}
+lemma_refl_proof :: Proof
+lemma_refl_proof = lemma_refl
+                *** QED
